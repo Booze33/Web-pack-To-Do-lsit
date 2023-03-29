70c173dd79913aa9c630492177fbd09e7cb03c05
Booze33 <tebetisloh@gmail.com>
2023-03-29 14:14:35 -0500

    added webpack

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import _ from 'lodash';
+import './style.css';
+
+/*<div class="input"><input type="checkbox" title="clear" class="checkbox">clean the car</div>
+        <button type="button" class="clear">Clear all completed</button>*/
+
+const tasks = [
+    { description: 'Work out', completed: false, index: 1 },
+    { description: "Download the resource", completed: false, index: 2 },
+    { description: "Clean your room", completed: true, index: 3 },
+    { description: "Study", completed: false, index: 4 },
+    { description: "Finish the app", completed: true, index: 5 },
+  ];
+
+  const taskList = document.querySelector(".input");
+  taskList.innerHTML = '';
+
+  const renderTasks = () => {
+    tasks.forEach((task) => {
+        const listItem = document.createElement("li");
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = task.completed;
+
+        listItem.appendChild(checkbox);
+        listItem.className = 'li-input';
+        listItem.appendChild(document.createTextNode(task.description));
+
+        taskList.appendChild(listItem);
+    })
+  }
+  renderTasks();
+
+  const clearAll = document.querySelector(".button");
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Clear all completed';
+  removeButton.className = "clear";
+  clearAll.appendChild(removeButton);
